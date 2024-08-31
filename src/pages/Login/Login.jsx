971cda0a2c87acb98d48d279eba524da75227ad4
Kasun_Udayanga <udayangakasun696@gmail.com>
2024-08-31 18:15:55 +0530

    add logo for main page

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
 import './Login.css'
+import  assets from '../../assets/assets'
 const Login = () => {
   return (
-    <div>Login</div>
+    <div className='login'>
+        <img src={assets.logo_big} alt="" className='logo' />
+    </div>
   )
 }
 
