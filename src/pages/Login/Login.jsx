3137f6f4a5132a944247b6db80f5193691fa8d61
Kasun_Udayanga <udayangakasun696@gmail.com>
2024-09-01 11:50:50 +0530

    add feature login page

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,12 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Login.css'
 import  assets from '../../assets/assets'
 const Login = () => {
+
+  const[currState,setCurrState]=useState("Sign Up");
   return (
     <div className='login'>
         <img src={assets.logo_big} alt="" className='logo' />
+        <form className='login-form'>
+          <h2>{currState}</h2>
+          {currState ==="Sign Up"?<input type="text" placeholder="Username" className='form-input'required/>:null}
+          <input type="email" placeholder="Email address" className='form-input'/>
+          <input type="password" placeholder=" Password" className='form-input'/>
+          <button type='submit'>{currState ==="Sign Up"?"Create account":"Login now"}</button>
+          <div className="login-term">
+            <input type="checkbox" />
+            <p>Agree to the terms of use & privacy policy</p>
+          </div>
+          <div className="login-forgot">
+            <p className="login-toggle">Already have an account <span onClick={()=>setCurrState("Login")}>click here</span></p>
+          </div>
+
+        </form>
     </div>
   )
 }
-
+//21.34
 export default Login
